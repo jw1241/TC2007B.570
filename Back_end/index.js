6de0bb4a0f19a2f78c1e8877fb0bacd7e53f6cba
Jianhan Wang <jianhanwang@MacBook-Pro-377.local>
2026-04-08 13:22:35 -0600

    index

--- Back_end/index.js
+++ Back_end/index.js
@@ -1,3 +1,25 @@
-require('dotenv').config();
+const express = require("express"); // Construir una API
+const dotenv = require('dotenv').config(); // Usar variables de .env
 
-console.log(process.env.API_KEY);
+dotenv.config();
+
+const app = express(); // Crear servidor
+app.use(express.json()); 
+
+app.use(express.json()); // Analizar automáticamente los datos JSON
+
+const itemRoutes = require("./routes/items"); // Importar archivo de ruta
+app.use("/api/items", itemRoutes);
+
+// Carga la UI de Swagger y API config
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./swagger");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+const PORT = process.env.PORT || 3000;
+
+//  Inicia back-end
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

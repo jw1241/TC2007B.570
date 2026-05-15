f742276ec2c21011472d9b80028dd38fd5302dfc
sg4m <moralessantiago8900@gmail.com>
2026-05-15 11:56:00 -0600

    feat: initialize project structure and implement UI templates for core application pages

--- Front_end/src/app/pages/inicio-resumen-alumno/inicio-resumen-alumno.page.ts
+++ Front_end/src/app/pages/inicio-resumen-alumno/inicio-resumen-alumno.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
   templateUrl: './inicio-resumen-alumno.page.html',
   styleUrls: ['./inicio-resumen-alumno.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, CommonModule, FormsModule]
 })
 export class InicioResumenAlumnoPage implements OnInit {
 

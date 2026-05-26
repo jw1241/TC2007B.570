665b6413637925c5784a693444573e0f66aff4f5
sg4m <moralessantiago8900@gmail.com>
2026-05-25 19:13:56 -0600

    feat: implement admin routes for user management and configure frontend development environment

--- Back_end/routes/adminRoutes.js
+++ Back_end/routes/adminRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
 const { supabaseAdmin } = require("../config/supabaseClient");
-const { supabaseAdmin } = require("../config/supabaseClient");
 
 /**
  * @swagger
